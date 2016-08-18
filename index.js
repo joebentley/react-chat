4d5afbf0f7ce4e9d4d917373ee564fc2fa49624c
Joe Bentley <joebentley10@gmail.com>
2016-08-18 09:11:30 +0100

    Changed port in server listening string

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express()
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.listen(8080, function () {
-  console.log('listening on port 3000')
+  console.log('listening on port 8080')
 })
