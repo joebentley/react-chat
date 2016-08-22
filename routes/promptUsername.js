ae2df66c5aef5be6565a63ee3e5c6dd77400097c
Joe Bentley <joebentley10@gmail.com>
2016-08-22 21:48:34 +0100

    Added warning if username empty

--- routes/promptUsername.js
+++ routes/promptUsername.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('prompt')
+  res.render('prompt', { error: '' })
 })
 
 router.post('/', function (req, res, next) {
+  if (req.body.username.match(/\S/g) === null) {
+    res.render('prompt', { error: 'Error: username cannot be empty' })
+    next()
+  }
+
   req.session.username = req.body.username
   res.redirect('/')
 })
